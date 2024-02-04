b46c578220a4c30a20a4edd2d9cfb51b5eb37d6c
bridgeharris <82836515+bridgeharris@users.noreply.github.com>
2024-02-04 13:28:57 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Tell me the story',
+      label: 'poachable',
     },
     {
       label: 'Redirect to cute dog pictures',
