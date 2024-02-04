06494fe95bbf2507e8d9005d30f1ab459648ad41
bridgeharris <82836515+bridgeharris@users.noreply.github.com>
2024-02-04 14:35:28 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const frameMetadata = getFrameMetadata({
       action: 'post_redirect',
     },
   ],
-  image: `${NEXT_PUBLIC_URL}/Coinbase-Logo-2012.png`,
+  image: `$public/Coinbase-Logo-2012.png`,
   input: {
     text: 'talk to me about crypto',
   },
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'zizzamia.xyz',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/Coinbase-Logo-2012.png`],
+    images: [`$public/Coinbase-Logo-2012.png`],
   },
   other: {
     ...frameMetadata,
